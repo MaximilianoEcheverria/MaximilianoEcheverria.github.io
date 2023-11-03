7198721ae0cd04b1c483342a2e730d3b6ba84ada
Maximiliano Echeverria <Maxiicarp88@gmail.com>
2023-11-03 04:32:33 -0300

    aplicamos a la pagina web responsive

---
+++ resouces/js/codigo.js
@@ -0,0 +1,9 @@
+//FUNCION QUE MUESTRA EL MUENU RESPONSIVE 
+function responsiveMenu(){
+    let x = document.getElementById("nav");
+    if(x.className === ""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
